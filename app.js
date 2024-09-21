0310c252ba3de7d75e0b6c194bf4d2c536ffa349
renad <renad@pursuit.org>
2024-09-21 14:04:16 -0400

    ADDED: lendersController.js to app.js

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
 const borrowersController = require("./controllers/borrowersController");
+const lendersController = require("./controllers/lendersController");
 
 /* Configuration */
 const app = express();
@@ -14,6 +15,7 @@ app.use(cors());
 app.use(express.json());
 app.use(morgan("tiny"));
 app.use("/borrowers", borrowersController);
+app.use("/lenders", lendersController);
 
 app.get("/", (req, res) => {
   res.status(200).json({ index: "Money Money Money." });
