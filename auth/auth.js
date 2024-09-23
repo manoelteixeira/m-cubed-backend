e449fdafee0fe0f9d62a7ca03f5c00de773b873e
manoelteixeira <manoel.teixeira.cc@gmail.com>
2024-09-23 12:44:33 -0400

    REMOVE: auth folder

--- auth/auth.js
+++
@@ -1,8 +0,0 @@
-// // auth/auth.js
-// const jwt = require("jsonwebtoken");
-// require("dotenv").config();
-// const secret = process.env.SECRET;
-
-// function authenticateToken(req, res, next){
-//     const token
-// }
