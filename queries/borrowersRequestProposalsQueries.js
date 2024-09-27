8ac3e7dcdcdc2594d63030300259c21de088e2f0
manoelteixeira <manoel.teixeira.cc@gmail.com>
2024-09-27 11:17:59 -0400

    ADD: borrowers request proposal accept route

--- queries/borrowersRequestProposalsQueries.js
+++ queries/borrowersRequestProposalsQueries.js
@@ -25,9 +25,40 @@ async function getProposal(request_id, id) {
   }
 }
 
-async function acceptProposal(params) {}
+async function acceptProposal(borrower_id, request_id, proposal_id) {
+  const currentDate = new Date();
+
+  const updateRequestQuery =
+    "UPDATE loan_requests SET funded_at=$[date], accepted_proposal_id=$[proposal_id] " +
+    "WHERE id=$[request_id] AND borrower_id=$[borrower_id] RETURNING *";
+
+  const updateProposals =
+    "UPDATE loan_proposals SET " +
+    "accepted = CASE WHEN id=$[proposal_id] THEN TRUE ELSE FALSE END " +
+    "WHERE loan_request_id=$[request_id] RETURNING *";
+
+  try {
+    const data = await db.tx(async (t) => {
+      const acceptedRequest = await t.one(updateRequestQuery, {
+        date: currentDate,
+        proposal_id: proposal_id,
+        borrower_id: borrower_id,
+        request_id: request_id,
+      });
+      const updatedProposals = await t.many(updateProposals, {
+        proposal_id: proposal_id,
+        request_id: request_id,
+      });
+      return { acceptedRequest, updatedProposals };
+    });
+    return data;
+  } catch (err) {
+    return err;
+  }
+}
 
 module.exports = {
   getProposals,
   getProposal,
+  acceptProposal,
 };
